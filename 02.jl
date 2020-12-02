9202f03254b69fda2904a6d25d74bcabdaab168e
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2020-12-02 01:04:28 -0500

    refactor for 02.2

--- 02.jl
+++ 02.jl
@@ -2,12 +2,16 @@
 
 input = readlines("02.input")
 
-function isvalid(line)
+function parse_line(line)
     policy, password = split(line, ": ")
     range, char = split(policy, ' ')
     low, high = parse.(Int, split(range, '-'))
-
-    n = sum(isequal(first(char)), cc for cc in password)
+    return low, high, first(char), password
+end
+    
+function isvalid(line)
+    low, high, char, password = parse_line(line)
+    n = sum(isequal(char), cc for cc in password)
     @show low, high, char, password, n
     return(low ≤ n ≤ high)
 end
@@ -15,3 +19,10 @@ end
 star1(input) = sum(isvalid, input)
 
 star1(input)
+
+function isvalid2(line)
+    low, high, char, password = parse_line(line)
+    return xor(password[low] == char, password[high] == char)
+end
+
+sum(isvalid2, input)
