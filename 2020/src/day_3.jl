930faf45f20aacacd0a33d0aca149702991100bb
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2020-12-03 21:31:01 -0500

    03.2 (and tiny refactor)

--- 2020/src/day_3.jl
+++ 2020/src/day_3.jl
@@ -5,10 +5,8 @@ input = readlines("2020/data/day_3.txt")
 
 wrap(idx, size) = rem((idx-1), size)+1
 
-function part_1(input)
+function part_1(input, down=1, across=3)
     size_v, size_h = length(input), length(first(input))
-
-    down, across = 1, 3
 
     vs = 1:down:size_v
     hs = (wrap(h, size_h) for h in range(1, step=across, length=length(vs)))
@@ -18,6 +16,6 @@ end
 @info part_1(input)
 
 function part_2(input)
-    nothing
+    prod(part_1.(Ref(input), (1,1,1,1,2), (1,3,5,7,1)))
 end
 @info part_2(input)
