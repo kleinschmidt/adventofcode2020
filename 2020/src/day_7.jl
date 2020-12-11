a3085c4c81549589ef7916f61960471be85ce441
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2020-12-10 20:04:59 -0500

    wip day 7

---
+++ 2020/src/day_7.jl
@@ -0,0 +1,18 @@
+# https://adventofcode.com/2020/day/7
+using AdventOfCode
+
+input = readlines("2020/data/day_7.txt")
+
+function parse_line(line)
+    m = match(r"([a-z ]*) bags? contain ([a-z0-9, ]*).", line)
+end
+
+function part_1(input)
+    nothing
+end
+@info part_1(input)
+
+function part_2(input)
+    nothing
+end
+@info part_2(input)
