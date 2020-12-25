6a93beaa29c2638ba3671b7dec78ecdf229f8bf8
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2020-12-24 21:10:53 -0500

    08.2 wip

--- 2020/src/day_8.jl
+++ 2020/src/day_8.jl
@@ -8,6 +8,9 @@ function parse_input(line)
     Symbol(inst), parse(Int, n)
 end
 
+ptr_offset(inst, n) = inst == :jmp ? n : 1
+acc_offset(inst, n) = inst == :acc ? n : 0
+
 function part_1(input)
     code = parse_input.(input)
     
@@ -17,23 +20,37 @@ function part_1(input)
     while ptr ∉ executed
         inst, n = code[ptr]
         push!(executed, ptr)
-        if inst == :acc
-            acc += n
-            ptr += 1
-        elseif inst == :jmp
-            ptr += n
-        elseif inst == :nop
-            ptr += 1
-        else
-            error("unknown instruction: $inst $n")
-        end
+        ptr += ptr_offset(inst, n)
+        acc += acc_offset(inst, n)
     end
     return acc
 end
 @info part_1(input)
 
+# control flow doesn't depend on the accumulator, just the pointer operations.
+# so we can treat this as a graph with (at least) two connected components.  we
+# need to find all the nodes that will reach the n+1th instruction and all the
+# ones that are reachable from the first
 
 function part_2(input)
-    nothing
+    code = parse_input.(input)
+
+    # a from × to matrix of edges
+    edges = falses(length(code), length(code)+1)
+
+    for (ptr, inst) in enumerate(code)
+        next_ptr = ptr + ptr_offset(inst...)
+        if next_ptr <= size(edges, 2)
+            edges[ptr, next_ptr] = true
+        end
+    end
+
+    colors = zeros(Int, length(code))
+
+    ptr = 1
+    while colors[ptr] == 0
+        colors[ptr] = 1
+        ptr = 
+    edges
 end
-@info part_2(input)
+part_2(input)
